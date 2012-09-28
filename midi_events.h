3f9e7f1c42347f9538147628a436977404f77d20
Florian Paul Schmidt <mista.tapas@gmx.net>
2012-09-29 00:31:31 +0200

    forgot a file

---
+++ midi_events.h
@@ -0,0 +1,40 @@
+#ifndef DOOB_MIDI_EVENTS_HH
+#define DOOB_MIDI_EVENTS_HH
+
+#include <types.h>
+#include <event.h>
+
+namespace doob {
+
+/**
+ * Base class for all kinds of events
+ */
+struct midi_event : event {
+	typedef unsigned int note_t;
+	typedef unsigned int velocity_t;
+	
+	typedef unsigned int cc_number_t;
+	typedef unsigned int cc_value_t;
+};
+
+typedef shared_ptr<midi_event> midi_event_ptr;
+
+struct midi_note_on_event : midi_event {
+	note_t note;
+	velocity_t velocity;
+};
+
+struct midi_note_off_event : midi_event {
+	note_t note;
+	velocity_t velocity;
+};
+
+struct midi_cc_event : midi_event {
+	cc_number_t number;
+	cc_value_t value;
+};
+
+} // namespace
+
+#endif
+
